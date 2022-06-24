7ec09444cb45d17b9f066c597199f10764c56a88
baltasn <nks.mpaltas@gmail.com>
2022-06-24 19:53:34 +0300

    stage 2
    If a user enters a task name to the input field and presses the button with the #add-task-button id, the program should wrap the task in <li> tags and add it to the end of the <ul> tag.
    
    The task should have the same elements as in the previous stage:
    
        <input> element of the checkbox type. The checkbox should be unchecked by default.
        <span> element with the task class. It contains the task name.
        <button> element with the delete-btn class. Remove the task from the list by clicking on this button.
    
    Once the task has been added, clear the input field so that a user can add another task.
    
    A task must have a name to be successfully added to a task list.
    
    Also, do not remove the hardcoded tasks from the previous stage.

---
+++ script.js
@@ -0,0 +1,86 @@
+"use strict";
+
+
+const newTaskInputField = document.getElementById("input-task");
+const btnAddTask = document.getElementById("add-task-button");
+const tasksEl = document.querySelector(".tasks");
+
+const taskList = [
+  // {task: "Email David", done: false},
+  // {task: "Create ideal user persona guide", done: false},
+  // {task: "Set up A/B test", done: false}
+];
+
+let tasksListEl;
+
+
+function addTaskToList() {
+  if(newTaskInputField.value !== ""){
+    taskList.push({task: newTaskInputField.value, done: false});
+    newTaskInputField.value = "";
+    tasksListEl = document.createElement("ul");
+    tasksListEl.setAttribute("id", "task-list");
+    tasksEl.replaceChildren(tasksListEl)
+    taskList.forEach(createTaskElement)
+    console.log(taskList);
+  }
+}
+
+function createTaskElement(task,taskIndex) {
+
+  const taskCheckBox = document.createElement("input");
+  taskCheckBox.setAttribute("class" ,"checkbox");
+  taskCheckBox.setAttribute("type" ,"checkbox");
+  taskCheckBox.setAttribute("id", `task-checkbox-${taskIndex}`);
+  taskCheckBox.checked = task.checked;
+
+  const taskText = document.createElement("span");
+  taskText.setAttribute("class", "task");
+  taskText.setAttribute("id", `task-${taskIndex}`);
+  taskText.innerHTML = task.task;
+
+  const taskDivEl = document.createElement("div");
+  taskDivEl.setAttribute("class", "task-div");
+  taskDivEl.appendChild(taskCheckBox);
+  taskDivEl.appendChild(taskText);
+
+
+  const btnDeleteTask = document.createElement("button");
+  btnDeleteTask.setAttribute("class", "delete-btn");
+  btnDeleteTask.setAttribute("id", `btn--delete-${taskIndex}`);
+  btnDeleteTask.addEventListener("click",function (){
+    taskList.splice(taskIndex, 1);
+    tasksListEl = document.createElement("ul");
+    tasksListEl.setAttribute("id", "task-list");
+    tasksEl.replaceChildren(tasksListEl)
+    taskList.forEach(createTaskElement)
+  });
+  btnDeleteTask.innerHTML = "delete";
+
+  const taskBtnEl = document.createElement("div");
+  taskBtnEl.setAttribute("class", "button-div");
+  taskBtnEl.appendChild(btnDeleteTask);
+
+
+  const taskItem = document.createElement("li");
+  taskItem.setAttribute("class", "task-item");
+  tasksListEl.appendChild(taskItem);
+  taskItem.appendChild(taskDivEl);
+  taskItem.appendChild(taskBtnEl);
+}
+
+
+
+btnAddTask.addEventListener("click", addTaskToList);
+
+
+newTaskInputField.value = "Email David";
+btnAddTask.click();
+
+newTaskInputField.value = "Create ideal user persona guide";
+btnAddTask.click();
+
+newTaskInputField.value = "Set up A/B test";
+btnAddTask.click();
+
+
